543884ef9c53b1da3081e007a96cfeeb5efb7248
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-07-14 03:09:45 +0800

    Split spigot post startup argument out of JVM argument
    Post args: the args send to spigot instead of JVM

--- BootServer.cmd
+++ BootServer.cmd
@@ -22,11 +22,14 @@ set nonProxyHosts="127.0.0.1"
 @REM https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/
 @set optimizedArgs=-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true
 
-@REM Set jar and post startup argument
-set jarArgs=-jar "%serverJar%" nogui
+@REM Set jar
+@set jarArgs=-jar "%serverJar%"
+
+@REM Set post startup argument(spigot runtime argument)
+@set postArgs=nogui
 
 :boot
-java %basicArgs% %networkArgs% %optimizedArgs% %jarArgs%
+java %basicArgs% %networkArgs% %optimizedArgs% %jarArgs% %postArgs%
 
 @echo Restart server after 15 seconds.
 @timeout /t 15

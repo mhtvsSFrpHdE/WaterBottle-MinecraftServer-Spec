6ed50a29cd93ae521e684aa592ec3c7a0c68b3bd
mhtvsSFrpHdE <2bemperor47@gmail.com>
2025-11-19 01:21:54 +0800

    Add no wait switch
    If NoWait specified, close server without alert players

--- RconStopOnBackup.ps1
+++ RconStopOnBackup.ps1
@@ -1,23 +1,28 @@
 ﻿param(
-    [string]$ServerFileName
+    [string]$ServerFileName,
+    [switch]$NoWait = $False
 )
 
 $serverInstanceCount = @(Get-WmiObject Win32_Process -Filter "name = 'java.exe'" | Where-Object CommandLine -match "$($ServerFileName)").Count
-#$serverInstanceCount = 0
 if ($serverInstanceCount -ne 0){
-    rcon "say §aBackup server data, restart in 120 seconds"
-    rcon "say 备份服务器数据，120秒后重启服务器"
-    Start-Sleep -Seconds 60
-    rcon "say §aServer restart in 60 seconds"
-    rcon "say 60秒后重启服务器"
-    Start-Sleep -Seconds 30
-    rcon "say §aServer restart in 30 seconds"
-    rcon "say 30秒后重启服务器"
-    Start-Sleep -Seconds 25
-    rcon "say §aServer restart in 5 seconds"
-    rcon "say 5秒后重启服务器"
-    Start-Sleep -Seconds 5
-    rcon "stop"
+    if ($NoWait -eq $False){
+        rcon "say §aBackup server data, restart in 120 seconds"
+        rcon "say 备份服务器数据，120秒后重启服务器"
+        Start-Sleep -Seconds 60
+        rcon "say §aServer restart in 60 seconds"
+        rcon "say 60秒后重启服务器"
+        Start-Sleep -Seconds 30
+        rcon "say §aServer restart in 30 seconds"
+        rcon "say 30秒后重启服务器"
+        Start-Sleep -Seconds 25
+        rcon "say §aServer restart in 5 seconds"
+        rcon "say 5秒后重启服务器"
+        Start-Sleep -Seconds 5
+        rcon "stop"
+    }
+    else{
+        rcon "stop"
+    }
 }
 if ($serverInstanceCount -eq 0){
     Write-Output "Server not running, skip rcon commands"
